165bcd3cda72b630fcd2ef2025b006caf21534a6
Damian <mort@Damians-MacBook-Pro.local>
2012-05-08 12:57:26 +0200

    Wersja początkowa.

---
+++ ToDo/NNPAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  NNPAppDelegate.h
+//  ToDo
+//
+//  Created by damian on 14/04/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ToDo.h"
+
+@interface NNPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) NSMutableArray *toDos;
+@property (strong, nonatomic) NSMutableArray *toDosCompleted;
+
+@end
